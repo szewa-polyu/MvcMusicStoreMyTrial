ca3a9a5cd2209ddb9c4fd3981aa10ddfe874c032
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2017-12-28 20:33:38 +0800

    Part 3 done
    finish part 2 amd 3
    https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/mvc-music-store/mvc-music-store-part-3

---
+++ MvcMusicStoreMyTrial/Controllers/StoreController.cs
@@ -0,0 +1,45 @@
+﻿using MvcMusicStoreMyTrial.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStoreMyTrial.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            IList genres = new List<Genre>()
+            {
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+            return View(genres);
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            Genre genreModel = new Genre
+            {
+                Name = genre
+            };
+            return View(genreModel);
+        }
+
+        // GET: Store/Details
+        public ActionResult Details(int id)
+        {
+            Album album = new Album
+            {
+                Title = "Album " + id
+            };
+            return View(album);
+        }
+    }
+}
