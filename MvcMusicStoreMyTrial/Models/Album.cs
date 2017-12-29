96a62dc087fb43d2507fdfaccbe19f7e16e02287
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2017-12-30 02:01:53 +0800

    Part 5 done
    https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/mvc-music-store/mvc-music-store-part-5

--- MvcMusicStoreMyTrial/Models/Album.cs
+++ MvcMusicStoreMyTrial/Models/Album.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMusicStoreMyTrial.Models
 {
     public class Album
     {
+        [Display(Name = "Album ID")]
         public int AlbumId { get; set; }
+
+        [Display(Name = "Genre ID")]
         public int GenreId { get; set; }
+
+        [Display(Name = "Artist ID")]
         public int ArtistId { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
+
+        [Display(Name = "Album Art URL")]
         public string AlbumArtUrl { get; set; }
+
         public Genre Genre { get; set; }
         public Artist Artist { get; set; }
     }
