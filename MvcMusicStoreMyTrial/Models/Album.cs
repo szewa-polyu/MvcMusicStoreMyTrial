ed5a88f8c432f86b0cfaf716c169e4b8b23d7ff1
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-01-05 00:59:40 +0800

    Parts 6, 7

--- MvcMusicStoreMyTrial/Models/Album.cs
+++ MvcMusicStoreMyTrial/Models/Album.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MvcMusicStoreMyTrial.Models
 {
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
-        [Display(Name = "Album ID")]
+        [ScaffoldColumn(false)]
         public int AlbumId { get; set; }
-
-        [Display(Name = "Genre ID")]
+        [Display(Name = "Genre")]
         public int GenreId { get; set; }
-
-        [Display(Name = "Artist ID")]
+        [Display(Name = "Artist")]
         public int ArtistId { get; set; }
+        [Required(ErrorMessage = "An Album Title is required")]
+        [StringLength(60)]
         public string Title { get; set; }
+        [Range(0.01, 100.00, ErrorMessage = "Price must be between 0.01 and 100.00")]
         public decimal Price { get; set; }
-
         [Display(Name = "Album Art URL")]
+        [StringLength(1024)]
         public string AlbumArtUrl { get; set; }
-
-        public Genre Genre { get; set; }
-        public Artist Artist { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
     }
 }
